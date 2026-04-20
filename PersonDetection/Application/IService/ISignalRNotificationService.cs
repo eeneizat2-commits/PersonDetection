d9ee27bfb8c589e1295f88ce89450b521ed3ec5d
eeneizat2-commits <e.eneizat2@gmail.com>
2026-04-20 04:55:17 -0700

    Updated .gitignore

--- PersonDetection/Application/IService/ISignalRNotificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonDetection.Application.IService
-{
-    public interface ISignalRNotificationService
-    {
-        Task NotifyNewVideoJob(Guid jobId, string fileName);
-        Task NotifyVideoProgress(Guid jobId, string fileName, int progress, int processedFrames, int totalPersons, int uniquePersons);
-        Task NotifyVideoComplete(Guid jobId, string fileName, string state, int totalPersons, int uniquePersons, double processingTime);
-    }
-}
